ab1a3652a43433661fbf8f58c210d03fb6b63d21
n0sko <wojtek.noskowiak@gmail.com>
2021-10-06 11:20:21 +0200

    poprawki

--- 0/main.py
+++ 0/main.py
@@ -3,8 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.arange(0,10,0.001)
-y = np.sin(x)
+y1 = np.sin(x)
+y2 = np.cos(x)
 
-graph = plt.plot(x,y)
+
+plt.plot(x,y1, color='r', linestyle=':')
+plt.plot(x,y2, color='g', linestyle='-')
+plt.title('wykres')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.savefig('wykres.png')
 plt.show()
