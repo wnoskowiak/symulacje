6d54f819cb362da810450837104135e2a073e3f7
n0sko <wojtek.noskowiak@gmail.com>
2021-10-19 17:39:35 +0200

    prezentacja finished

--- 1/3.py
+++ 1/3.py
@@ -9,15 +9,16 @@ def gauss(x,t,D):
 D=2
 t=100
 N=100000
+bins = 100
   
 brownian_motions=np.sqrt(2*D)*get_brown.get_brownian_motion(t,N)
 trajectories=np.cumsum(brownian_motions,axis=0)
 x=np.linspace(-100,100,10000)
 
 fig = plt.figure()
-ax1 = plt.axes(ylim=[0,0.1])
+ax1 = plt.axes(ylim=[0,0.1],xlim=[-40,40])
 plt.axvline(0,color='black', linewidth=1, alpha=0.75)
-hist = ax1.hist([],bins=50,alpha=0.75,density=True, color='green', animated=True)
+hist = ax1.hist([],bins=bins,alpha=0.75,density=True, color='green', animated=True)
 title = ax1.text(0.93,.9, "", bbox={'facecolor':'w', 'alpha':0.5, 'pad':5},
                 transform=ax1.transAxes, ha="center")
 gss, = ax1.plot([],[],color='black', linewidth=1, alpha=0.75)
@@ -25,32 +26,19 @@ gss, = ax1.plot([],[],color='black', linewidth=1, alpha=0.75)
 lines = [hist[-1], gss]
 
 def init():
-    temp1 = ax1.hist(trajectories[0],bins=50,alpha=0.75,density=True, color='green')[-1]
+    temp1 = ax1.hist(trajectories[0],bins=bins,alpha=0.75,density=True, color='green')[-1]
     lines[1].set_data(x,gauss(x,0,D))
     title.set_text("0")
     return [lines[1],*temp1, title]
 
 def animate(frame):
-    temp1 = ax1.hist(trajectories[frame],bins=50,alpha=0.75,density=True, color='green')[-1]
+    temp1 = ax1.hist(trajectories[frame],bins=bins,alpha=0.75,density=True, color='green')[-1]
     lines[1].set_data(x,gauss(x,frame,D))
     title.set_text(str(frame))
     return [lines[1],*temp1, title]
 
-
-"""
-for i in range(1,25):
-    time = int((i*t)/10)
-    fig=plt.figure()
-    plt.xlim([-100,100])
-    plt.ylim([0,0.1])
-    plt.hist(trajectories[time],bins=50,alpha=0.75,density=True, color='green')
-    plt.plot(x,gauss(x,time,D),color='black', linewidth=1, alpha=0.75)
-    plt.show()
-
-"""
-
 anim = animation.FuncAnimation(
-    fig, animate, init_func=init, frames=99, interval=0.01, blit=True
+    fig, animate, init_func=init, frames=99, interval=100, blit=True
 )
 
 plt.show()
--- 1/4.py
+++ 1/4.py
@@ -4,22 +4,40 @@ import get_brown
 from matplotlib import animation
 
 D=2
-N=100
+N=1500
 t=1000
 
-fig = plt.plot()
-x=2*D*np.linspace(0,t-1,t)
-ax1,  = plt.plot(x)
+brownian_motions=np.sqrt(2*D)*get_brown.get_brownian_motion(t,N)
+trajectories=np.cumsum(brownian_motions,axis=0)
+trajectories=np.transpose(trajectories)
 
+data = []
+
+for i in range(1,int(N/10)):
+    sett = trajectories[:(10*i)]
+    deviation=sum(sett*sett)/(10*i)
+    data.append(deviation)
+
+y = 2*D*np.linspace(0,t-1,t)
+x = np.arange(len(y))
+
+fig = plt.figure()
+ax1  = plt.axes(xlim=(0,1000), ylim=(0,4000))
+const, = ax1.plot(x,y)
+other, = ax1.plot(x,data[0])
+
+
+title = ax1.text(0.93,.1, "", bbox={'facecolor':'w', 'alpha':0.5, 'pad':5},
+                transform=ax1.transAxes, ha="center")
+
+lines = [other, title]
 def animate(frame):
-    brownian_motions=np.sqrt(2*D)*get_brown.get_brownian_motion(t,frame)
-    trajectories=np.cumsum(brownian_motions,axis=0)
-    trajectories=np.transpose(trajectories)
-    deviation=sum(trajectories*trajectories)/frame
-    plt.plot(deviation)
+    lines[0].set_data(x,data[frame])
+    lines[1].set_text(str(frame*10))
+    return lines
 
 anim = animation.FuncAnimation(
-    fig, animate, frames=99, interval=0.01
+    fig, animate, frames=148, interval=30
 )
 
 plt.show()
